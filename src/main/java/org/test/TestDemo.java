1669c076b673e73af0d136913d06b59407df9374
DongYinhang <13023695951@163.com>
2021-04-23 14:06:58 +0800

    22

---
+++ src/main/java/org/test/TestDemo.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class TestDemo {
+
+    public static void main(String[] args) {
+        String s = "123";
+        String s1 = "123";
+        String s2 = "123";
+        String s3 = "123";
+        String s4 = "123";
+        String s5 = "123";
+    }
+}
